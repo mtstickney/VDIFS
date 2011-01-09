e5cecadf78bcbd2d24b9bb4cc8ca5eb494b416db
Matthew Stickney <fiendishlinux@gmail.com>
2011-01-08 20:12:58 -0500

    Use address_space ops for file reads/writes

--- inode.c
+++ inode.c
@@ -2,6 +2,7 @@
 #include <linux/buffer_head.h>
 #include <linux/types.h>
 #include <linux/dcache.h>
+#include <linux/mpage.h>
 #include "vdifs.h"
 
 static struct file_operations vdifs_file_ops = {
@@ -13,6 +14,62 @@ static struct file_operations vdifs_file_ops = {
 	.mmap = generic_file_mmap
 };
 
+int vdifs_write_page(struct page*, struct writeback_control*);
+int vdifs_read_page(struct file*, struct page*);
+
+static struct address_space_operations vdifs_aops = {
+	.writepage = vdifs_write_page,
+	.readpage = vdifs_read_page
+};
+
+int vdifs_get_block(struct inode *inode, sector_t iblock, 
+	struct buffer_head *bh_result, int create)
+{
+	int32_t block_start;
+	sector_t logical_block;
+	struct super_block *sb;
+	
+	sb = inode->i_sb;
+	if (iblock > VDIFS_SB(inode->i_sb)->disk_blocks)
+		goto out;
+	if (VDIFS_SB(sb)->image_type == VDI_STATIC) {
+		block_start = iblock * sb->s_blocksize + VDIFS_SB(sb)->block_offset;
+		return 0;
+	} else {
+		/* assuming sparse vdi format */
+		block_start = VDIFS_SB(sb)->blockmap[iblock];
+		if (block_start < 0) {
+			if (!create)
+				goto out;
+			VDIFS_SB(sb)->alloced_blocks++;
+			block_start = VDIFS_SB(sb)->block_offset;
+			block_start += VDIFS_SB(sb)->alloced_blocks*sb->s_blocksize;
+			VDIFS_SB(sb)->blockmap[iblock] = block_start;
+			/* FIXME: assumes FS image block alignment */
+			logical_block = block_start / sb->s_bdev->bd_block_size;
+			map_bh(bh_result, sb, logical_block);
+			memset(bh_result->b_data, 0, bh_result->b_size);
+			mark_buffer_dirty(bh_result);
+			return 0;
+		}
+		logical_block = block_start / sb->s_bdev->bd_block_size;
+		map_bh(bh_result, sb, logical_block);
+		return 0;
+	}
+out:
+	return 1;
+}
+
+int vdifs_write_page(struct page *page, struct writeback_control *wbc)
+{
+	return mpage_writepage(page, vdifs_get_block, wbc);
+}
+
+int vdifs_read_page(struct file *file, struct page *page)
+{
+	return mpage_readpage(page, vdifs_get_block);
+}
+
 struct inode *vdifs_make_inode(struct super_block *sb, int mode)
 {
 	struct inode *inode;
@@ -22,9 +79,11 @@ struct inode *vdifs_make_inode(struct super_block *sb, int mode)
 		return NULL;
 	inode->i_mode = mode;
 	inode->i_uid = inode->i_gid = 0;
-	inode->i_blkbits = sb->s_blocksize_bits;
+	/* looks like this is set with inode_init_always() */
+	/* inode->i_blkbits = sb->s_blocksize_bits; */
 	inode->i_blocks = VDIFS_SB(sb)->disk_blocks;
 	inode->i_mtime = inode->i_ctime = inode->i_atime = CURRENT_TIME;
+	inode->i_mapping->a_ops = &vdifs_aops;
 	return inode; 
 }
 
